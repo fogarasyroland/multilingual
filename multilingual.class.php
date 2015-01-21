b5c59f68c9ef97bc06903007764c32ba404b8bfd
Roli <fogarasy.roland@gmail.com>
2015-01-21 18:31:48 +0100

    Létrehozás

---
+++ multilingual.class.php
@@ -0,0 +1,17 @@
+<?php
+class Multilingual {
+	protected $languages;
+	protected $language_file;
+
+	function __construct($selected_language,$languages){
+		$this->languages = $languages;
+		$this->language_file = (in_array($selected_language,array_keys($languages)))?$this->languages[$selected_language]:reset($this->languages);
+		ob_start(array($this,'translate'));
+	}
+	
+	function translate($buffer){
+		$language_data = parse_ini_file($this->language_file);
+		return (str_replace(array_keys($language_data),array_values($language_data), $buffer));
+	}
+}
+?>
